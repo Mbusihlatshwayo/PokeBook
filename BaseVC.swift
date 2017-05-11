6e25f4abb999e1f005e9b1313c82a25078259dc9
Mbusi Hlatshwayo <mbusi@cox.net>
2017-05-10 23:14:55 -0700

    added new font and audio play functionality

--- BaseVC.swift
+++ BaseVC.swift
@@ -7,20 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class BaseVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var collectionView: UICollectionView!
     var pokemonArray = [Pokemon]()
+    var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
         collectionView.dataSource = self
         parseCSV()
-        
+        initAudio()
     }
     
+    @IBAction func musicButtonPressed(_ sender: UIButton) {
+        if audioPlayer.isPlaying {
+            audioPlayer.pause()
+            sender.alpha = 0.8
+        } else {
+            audioPlayer.play()
+            sender.alpha = 1.0
+        }
+    }
+    
+    func initAudio() {
+        let path = Bundle.main.path(forResource: "music", ofType: "mp3")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(string: path!)!)
+            audioPlayer.prepareToPlay()
+            audioPlayer.numberOfLoops = -1
+            audioPlayer.play()
+        } catch let error as NSError {
+            print(error.debugDescription)
+        }
+    }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCollectionViewCell{
             let pokemon = pokemonArray[indexPath.row]
