4a8df7ec1ae80a59480adb73abdb3b6f59830820
Mbusi Hlatshwayo <mbusi@cox.net>
2017-05-10 21:04:37 -0700

    added images and csv file

---
+++ PokeBook/Pokemon.swift
@@ -0,0 +1,14 @@
+//
+//  Pokemon.swift
+//  PokeBook
+//
+//  Created by Mbusi Hlatshwayo on 5/10/17.
+//  Copyright © 2017 RainbowApps. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    
+}
